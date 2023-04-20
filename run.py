e821e2c62127ec834f515219a7056dce2c56fe97
LewisMDillon <lewismdillon1@gmail.com>
2023-04-20 12:29:17 +0000

    Ad function to emulate typing in console

--- run.py
+++ run.py
@@ -5,3 +5,19 @@
 import time
 import sys
 import art
+
+TITLE = art.TITLE
+
+
+def type_print(text, speed=0.03):
+    '''
+    Function for typewriter effect for printing the text
+    with variable speed, 0.03 speed by default
+    '''
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(speed)
+
+
+type_print(TITLE, 0.002)
