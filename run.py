8d21845c158af419a1604abc28957758058aed2c
LewisMDillon <lewismdillon1@gmail.com>
2023-04-20 20:28:38 +0000

    Add placeholer info to all other rooms in room_map

--- run.py
+++ run.py
@@ -1,13 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
 import time
 import sys
+import os
 import art
 
 
-def type_print(text, speed=0.03):
+def type_effect(text, speed=0.04):
     '''
     prints out text letter by letter, adjust speed argument
     to change speed, lower is faster
@@ -23,7 +20,7 @@ def display_title_screen():
     Displays the Main Game Logo
     """
     TITLE = art.TITLE
-    type_print(TITLE, 0.0001)
+    type_effect(TITLE, 0.0001)
 
 
 def display_main_menu():
@@ -31,7 +28,7 @@ def display_main_menu():
     Displays the main menu
     """
     MAIN_MENU = art.MAIN_MENU
-    type_print(MAIN_MENU, 0.0001)
+    type_effect(MAIN_MENU, 0.0001)
 
 
 # Player Setup ############
@@ -40,9 +37,9 @@ class Player:
         self.name = name
         self.health = health
         self.location = location
-        
-        
-myPlayer = Player('Lewis', 3, 'a1')
+
+
+myPlayer = Player('Name', 5, 'a1')
 
 
 def title_screen_options():
@@ -64,11 +61,9 @@ def title_screen_options():
         title_screen_options()
 
 
-# def update_player_location(location):
-
-
 def start_game():
     print('Game Starting...')
+    player_setup()
     # update_player_location(location)
     # update_player_health(health)
     # game_introduction()
@@ -99,13 +94,6 @@ def test_function():
     print(room_map[myPlayer.location]['description'])
     myPlayer.location = 'a4'
     print(room_map[myPlayer.location]['description'])
-
-
-def main():
-    display_title_screen()
-    time.sleep(0)
-    display_main_menu()
-    title_screen_options()
 
 
 # Map ########
@@ -158,6 +146,86 @@ room_map = {
         'east': 'a2',
         'west': False
     },
+    'b1': {
+        'room_name': 'Bedroom',
+        'description': 'a bedroom',
+        'details': 'you look around and see beds',
+        'completed': False,
+        'north': False,
+        'south': 'b1',
+        'east': 'a2',
+        'west': False
+    },
+    'b2': {
+        'room_name': 'Bathroom',
+        'description': 'a bathroom',
+        'details': "you look around and see a toilet",
+        'completed': False,
+        'north': False,
+        'south': 'b1',
+        'east': 'a2',
+        'west': False
+    },
+    'b3': {
+        'room_name': 'Swimming Pool',
+        'description': 'a Swimming Pool',
+        'details': "you look around and see a pool",
+        'completed': False,
+        'north': False,
+        'south': 'b1',
+        'east': 'a2',
+        'west': False
+    },
+    'b4': {
+        'room_name': 'Study',
+        'description': 'a Study',
+        'details': 'you look around and see a desk',
+        'completed': False,
+        'north': False,
+        'south': 'b1',
+        'east': 'a2',
+        'west': False
+    },
+    'c1': {
+        'room_name': 'Observatory',
+        'description': 'an Observatory',
+        'details': 'you look around and see space',
+        'completed': False,
+        'north': False,
+        'south': 'b1',
+        'east': 'a2',
+        'west': False
+    },
+    'c2': {
+        'room_name': 'Library',
+        'description': 'a library',
+        'details': "you look around and see books",
+        'completed': False,
+        'north': False,
+        'south': 'b1',
+        'east': 'a2',
+        'west': False
+    },
+    'c3': {
+        'room_name': 'Starting Room',
+        'description': 'the starting room',
+        'details': "you look around and see the starting room",
+        'completed': False,
+        'north': False,
+        'south': 'b1',
+        'east': 'a2',
+        'west': False
+    },
+    'c4': {
+        'room_name': 'Spike Pit',
+        'description': 'a spike pit',
+        'details': 'you look around and see spikes',
+        'completed': False,
+        'north': False,
+        'south': 'b1',
+        'east': 'a2',
+        'west': False
+    },
 }
 
 
@@ -171,6 +239,21 @@ def player_setup():
     os.system("clear")
     type_effect(f"Welcome, {myPlayer.name}. \n")
     update_player_location('c3')
+
+
+def update_player_location(destination):
+    """
+    Sets player location value to value of
+    destination, passed as an argument.
+    """
+    myPlayer.location = destination
+
+
+def main():
+    display_title_screen()
+    time.sleep(0)
+    display_main_menu()
+    title_screen_options()
 
 
 main()
