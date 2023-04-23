f5239e70a702297ba125f45d48ee7c065d5653ee
LewisMDillon <lewismdillon1@gmail.com>
2023-04-23 23:32:12 +0000

    Add gametext file to hold game block-text

---
+++ gametext.py
@@ -0,0 +1,128 @@
+room_descriptions = {
+
+    'a4': "Dining room. A fire burns in the huge hearth on the"
+          " opposite wall, casting warm orange light on to the"
+          " centerpiece of the room:\na gigantic oaken dining table.\n\n"
+          " On this dining table sits a single plate, upon which"
+          " rests the most, soft, warm, appetizing loaf of bread\n"
+          " Your stomach grumbles...",
+
+    'b3': "storage room. Wooden crates are piled high"
+          " all around you. \nThere is very little light,"
+          " but you can just make out a spiral staircase"
+          " to the east\n",
+
+    'b4': "Grand hall. your eyes follow the intricate pattern on the"
+          " red and gold carpet up to the staggeringly high walls,"
+          " adorned from bottom to top with beautiful paintings"
+          " and portraits.\n\n There are large ornate wooden doors"
+          " to the north and to the south.",
+
+    'c3': "Cold, stone room. It seems to be a cell of "
+          "some sort\nbut the large iron-barred door "
+          "to the north hangs open.\n",
+
+    'c4': "Narrow, stone-walled corridor. A single, small lantern"
+          " hangs from the curved roof, casting a dim light onto the"
+          " floor below, upon which you see a puddle has formed,"
+          " presumably from a leak in the stone ceiling."
+          " Through the hanging lantern's dim light, you also"
+          " see a strange door on the south end of the corridor.",
+
+}
+
+room_details = {
+
+    'a4': "Your eyes are transfixed on the loaf of bread.\n\n",
+
+    'b3': "You search around in the dark and find a RUSTY DAGGER!\n"
+          "It looks like decades of rust have blunted its blade, "
+          "but it's better than your fists.\n\n"
+          "**You equip the RUSTY DAGGER**\n\n"
+          "There is probably more hidden among the crates, but "
+          "it's too dark to see anything",
+
+    'b4': "You inspect the paintings. They are all beautifully done."
+          " You notice that the characters in the portraits are never"
+          " looking straight out, as you would expect, but instead"
+          " looking up or down at other portraits...\n\n"
+          " You follow the gaze of one portrait to the next, and again"
+          " and again and again \nuntil finally coming to a portrait"
+          " of a young girl.\n"
+          " The girl is holding out her hands as if presenting something"
+          " but her hands are empty. \n\n"
+          " As you peer into the painting, you realise that there is a"
+          " voice coming from it!\n"
+          " You press your ear close to it and try to make out what"
+          " it is saying...\n\n\n"
+          "     'Return to me when you can see beyond what is shown'\n\n\n",
+
+    'c3': "It's too dark to see anything."
+          " Perhaps if you had a light source...",
+
+    'c4': "You take a few steps forward up to the edge of the"
+          " puddle. You look up at the lantern above, and notice that"
+          " the liquid is coming from directly above the lantern. Not"
+          " only that, but the liquid seems to have corroded the lantern's"
+          " chain, which looks like it could come off if given a tug..."
+          " \nYou think you might be able to reach the lantern, but"
+          " if you slip, you might end up falling straight into the"
+          " puddle of strange, corrosive liquid.",
+
+}
+
+room_details_looked = {
+    'b3': "You search around in the dark but don't find anything"
+          "There is probably more hidden among the crates, but "
+          "it's too dark to see anything",
+}
+
+room_details_lantern = {
+    'b3': "With the aid of your lantern you search around the crates"
+          " It seems to be mostly useless junk, but at the bottom of"
+          " an old, rotting wooden crate you find a rolled-up scroll of"
+          " parchment. You carefully unravel and examine it. It's a map"
+          " of Fell Manor!"
+          "\n\n You can now type 'map' when prompted to see a map of"
+          " Fell Manor's rooms."
+}
+
+room_details_combined = {
+    'b3': "You search around and find a RUSTY DAGGER!\n"
+          "It looks like decades of rust have blunted its blade, "
+          "but it's better than your fists.\n\n"
+          "**You equip the RUSTY DAGGER**\n\n"
+          "Not only that, but with the aid of your lantern"
+          " you find, at the bottom of an old, rotting"
+          " wooden crate, a rolled-up scroll of parchment."
+          " You carefully unravel and examine it. It's a map"
+          " of Fell Manor!"
+          "\n\n You can now type 'map' when prompted to see a map of"
+          " Fell Manor's rooms."
+}
+
+item_text = {
+
+    'lantern_success': "You shuffle forward as close to the puddle as"
+                       " you dare. You reach your arm up as high as you can"
+                       " but you only just brush the underside of the lantern"
+                       " with your fingertips."
+                       " You steel yourself and try one more time, the liquid"
+                       " on the ground sizzles the toe of your right boot and"
+                       " you grimace and reach with all your might, until it"
+                       " feels as if your arm will separate from your shoulder"
+                       " and you grab it! With a quick sharp tug the lantern"
+                       " breaks off from its chain and you step carefully back"
+                       " from the still sizzling liquid, admiring your new"
+                       " handheld light source",
+
+    'lantern_failure': "You step forward to the edge of the puddle and reach"
+                       " as high as you can. Your are just about to reach the"
+                       " lantern when your foot slips and you stumble forward"
+                       " into the puddle of corrosive liquid!"
+                       " You leap backwards as fast as you can but the acid"
+                       " burns your feet and hands!"
+                       "\nYou take 1 point of damage"
+}
+
+
