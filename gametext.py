09394bc55e0b94feacea3c237024281cfa0d3294
LewisMDillon <lewismdillon1@gmail.com>
2023-04-24 20:13:12 +0000

    Move block text to gametext.py & create links

--- gametext.py
+++ gametext.py
@@ -1,11 +1,29 @@
 room_descriptions = {
 
+    'a1': "Luxurious study. The room's many shelves are home to"
+          " various odd trinkets which you've never seen before."
+          " You suddenly feel like something to the north moved"
+          " ever so slightly. You whip your gaze around to that side"
+          " of the room but everything lies still...",
+
+    'a2': "Pitch-dark chamber. You cannot see anything more than a"
+          " few inches in front of your face, but the sound of your"
+          " footsteps reverberating around the chamber gives you the"
+          " sense that this chamber is vast.",
+
+    'a3': "Library. Rows of books line shelves on the northern and"
+          " southern walls, all covered in a thick layer of dust.",
+
     'a4': "Dining room. A fire burns in the huge hearth on the"
           " opposite wall, casting warm orange light on to the"
           " centerpiece of the room:\na gigantic oaken dining table.\n\n"
           " On this dining table sits a single plate, upon which"
           " rests the most, soft, warm, appetizing loaf of bread\n"
           " Your stomach grumbles...",
+
+    'b1': "Long candlelit corridor. The candles' glow illuminates a"
+          " door at the southern end of the corridor, and you can see"
+          " an alcove on the western side with another door.",
 
     'b3': "storage room. Wooden crates are piled high"
           " all around you. \nThere is very little light,"
@@ -17,6 +35,12 @@ room_descriptions = {
           " adorned from bottom to top with beautiful paintings"
           " and portraits.\n\n There are large ornate wooden doors"
           " to the north and to the south.",
+
+    'c1': "Grand entranceway. In front of you stands a huge wooden door,"
+          " beatifully carved with intricate flowing designs from bottom"
+          " to top. Your eyes follow the lines from one design to the next"
+          " and then, as you take in the door as a whole, you realise that"
+          " all the carvings come together to form the outline of... a face?",
 
     'c3': "Cold, stone room. It seems to be a cell of "
           "some sort\nbut the large iron-barred door "
@@ -33,7 +57,33 @@ room_descriptions = {
 
 room_details = {
 
+    'a1': "You step nervously towards the north side of the room"
+          " and see, on the ground, a large sturdy wooden chest."
+          " you know that it can't have been the chest that moved"
+          " but at the same time, something about it looks... off",
+
+    'a2': "You take your first step forward into the blackness."
+          " Your right foot meets the solid ground below. You"
+          " step again but this time your left foot never touches"
+          " the ground. You tumble down through the empty dark"
+          " before meeting the ground, and your end, at the bottom",
+
+    'a3': "As you walk forward, eyes scanning along the countless"
+          " old tomes lining the walls, you see one book whose"
+          " cover does not seem to have any dust on it at all."
+          " You reach over and open it, but cannot read any of"
+          " its contents as it seems to be written in a strange script"
+          " that you have never seen before..."
+          "\n You leave the book back where you found it for now.",
+
     'a4': "Your eyes are transfixed on the loaf of bread.\n\n",
+
+    'b1': "As you make your way down the corridor, you notice that"
+          " there is a small brass ring hanging from the wall."
+          " You give it a slight pull and a little drawer pulls"
+          " out from the wall! Inside this drawer sits, a sealed"
+          " vial of thick blue liquid. It looks unnatural but you"
+          " are tempted to try it.",
 
     'b3': "You search around in the dark and find a RUSTY DAGGER!\n"
           "It looks like decades of rust have blunted its blade, "
@@ -78,6 +128,19 @@ room_details_looked = {
 }
 
 room_details_lantern = {
+
+    'a2': "You say a silent thanks for your lantern as you feel the"
+          " oppresive dark pushed back from you, even if only slightly."
+          " you take a ginger step forwards and lower your lantern to"
+          " see that the ground one step beyond your feet gives way"
+          " to a black chasm. Stepping beyond where you stand seems"
+          " like certain death, but you notice to your left, an"
+          " extremely narrow stone path, winding its way left and right"
+          " out across the chasm. You take the most careful steps you"
+          " have ever taken and manage to follow the stone path to"
+          " the other side of the room."
+          " \n\n **The west side of this room is now reachable**\n\n",
+
     'b3': "With the aid of your lantern you search around the crates."
           " It seems to be mostly useless junk, but at the bottom of"
           " an old, rotting wooden crate you find a rolled-up scroll of"
@@ -155,7 +218,54 @@ item_text = {
                        " into the puddle of corrosive liquid!"
                        " You leap backwards as fast as you can but the acid"
                        " burns your feet and hands!"
-                       "\nYou take 1 point of damage"
+                       "\nYou take 1 point of damage",
+
+    'health_potion': "The lid unscrews easily and you immediately"
+                     " and a sickly sweet aroma floats out of the vial."
+                     " You raise the vial to your lips and let the liquid"
+                     " pour into your mouth. As soon as you've finished it"
+                     " a strange feeling comes over you. You feel stronger,"
+                     " healthier, refreshed - as if you've had a really good"
+                     " night's sleep. You shake yourself out and continue on"
+                     " feeling a tiny bit more able to take on the challenges"
+                     " of this awful place."
+                     "\n\n**Your health increases by 5 points**\n\n"
 }
 
+enemy_text = {
 
+      'ogre': "You suddenly hear a loud crash to your left. A hulking,"
+              " green ogre stands in the doorway. You both stand frozen"
+              " for a brief second before it bellows out in a deafening"
+              " guttural roar:"
+              "\n\nMY BREEEEEEAD!!!!!\n\n"
+              " It bounds towards you and attacks!",
+
+      'haunted_chest': "You reach down to open the chest, but before you"
+                       " touch the latch, it springs open! Two encrusted"
+                       " eyes snap open from lines in the wooden grain"
+                       " and the chest leaps up at you, attempting to"
+                       " clamp down on you with the hundreds of tiny teeth"
+                       " that line the inside of its lid."
+}
+
+enemy_death = {
+
+      'ogre': "You breath heavily and look around the room."
+              " You don't know quite how you did that but you're"
+              " thankful you're alive. Since the ogre won't be needing"
+              " it anymore, you help yourself to the entire rest"
+              " of the breadloaf: a well earned reward."
+              "\n\n **Your health increases by 10 points!**\n\n",
+
+      'haunted_chest': "The chest lies still, lid hanging open lifelessly."
+                       " You wipe the sweat from your face and cautiously"
+                       " peer into the body of the chest, where, amongst"
+                       " broken bones, lies an ornate iron shield."
+                       " You slowly reach your hand inside, grasp the shield"
+                       " and quickly pull it out."
+                       "\n\n **You equip the IRON SHIELD**\n\n"
+                       "This thing looks like it offers some serious"
+                       " protection!"
+
+}
